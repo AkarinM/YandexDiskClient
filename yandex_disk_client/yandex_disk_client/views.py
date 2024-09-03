12b1ef8677242667bee2783bc9e0b91853b7d398
akarin <mihailmoro96@yandex.ru>
2024-09-03 22:07:05 +0300

    Чистка кода

--- yandex_disk_client/yandex_disk_client/views.py
+++ yandex_disk_client/yandex_disk_client/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.conf import settings
 
-from django.http import HttpResponse
 
-
-def index(request):
-    context = {
-        # 'client_id': settings.YAD_APP_CLIENTID,
+def index(request: 'HttpRequest') -> 'HttpResponse':
+    """
+    Отображает главную страницу
+    :param request:
+    :return:
+    """
+    context: dict = {
         'url_path':f'https://oauth.yandex.ru/authorize?response_type=token&client_id={settings.YAD_APP_CLIENTID}',
-        # 'origin_path': 'https://oauth.yandex.ru',
     }
 
 
